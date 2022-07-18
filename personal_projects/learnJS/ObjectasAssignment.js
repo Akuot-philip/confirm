2ca70316816aff5563da3808d80adee11a9013df
Akuot-philip <akuotphilipp@gmail.com>
2022-07-18 17:06:30 +0300

    corrections

--- personal_projects/learnJS/ObjectasAssignment.js
+++ personal_projects/learnJS/ObjectasAssignment.js
@@ -1,34 +1,57 @@
-var  pricelist={
-    orange:20,
-    mango:30,
-    apple:25,
-    berries:120
-}
-function calculateFruit(fruitname,quantinty){
-var price = pricelist[fruitname]
+// var  pricelist={
+//     orange:20,
+//     mango:30,
+//     apple:25,
+//     berries:120
+// }
+// function calculateFruit(fruitname,quantinty){
+// var price = pricelist[fruitname]
 
-  console.log(`${quantinty} ${fruitname} for KES ${price * quantinty}`);
+//   console.log(`${quantinty} ${fruitname} for KES ${price * quantinty}`);
 
+// }
+// calculateFruit('orange',2)
+
+
+//correction for number one
+
+class pricelist {
+  constructor(priceunit) {
+      this.priceunit = priceunit;
+      this.calculateFruitCost = (fruitName, quantity) => {
+          return `${quantity} ${fruitName} for KES ${parseFloat(this.priceunit*quantity).toFixed(1)}`
+      }      
+  }
 }
-calculateFruit('orange',2)
+let oranges = new pricelist(30.0)
+let pineapples = new pricelist( 50.0)
+console.log(oranges.calculateFruitCost('Mango', 2))
+console.log(pineapples.calculateFruitCost('Apple', 5))
+
+
 
 class KioskCalc{
   constructor(fruit,quantinty2){
     this.fruit=fruit;
     this.quantinty2=quantinty2;
     this.fruitPricelist={
-      'orang':30,
+      'orange':30,
       'mango':15,
-      'avocado':40
+      'avocad':40
 
     };
      
-  this.total=function(){
-    return`${this.quantinty2} ${this.fruit} for KES ${quantinty2 * this.fruitPricelist.orang}`
+  this.total=()=>{
+    return`${this.quantinty2} ${this.fruit} for KES ${quantinty2 * this.fruitPricelist.orange}`
   }
 
 }
 }
 
 var kioskCalc=new KioskCalc("orange",5)
+var kioskCalc2=new KioskCalc("mango",4)
+var kioskCalc3=new KioskCalc("Avocado",3)
+
 console.log(kioskCalc.total());
+console.log(kioskCalc2.total());
+console.log(kioskCalc3.total());
